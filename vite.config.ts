c8cc329849f36de27dba11c177117ea521b56de1
Jay Neill <jayneill@Jays-MacBook-Pro.local>
2025-10-08 01:27:03 -0500

    Configure GitHub Pages (vite base + gh-pages)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/jayneill-cs255-portfolio/',    // 👈 add this line
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
